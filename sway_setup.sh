c63f22e2f28cd1a3712f5fe06d037211a0fa7902
Rodolfo Souza <rodhfr@Brotesco.lan.net>
2024-10-24 09:30:41 -0800

    add sway_setup.sh unorganized

---
+++ sway_setup.sh
@@ -0,0 +1,16 @@
+sway
+vim
+wayland_protocols
+wayvnc
+htop
+fuzzel
+flatpak
+# need to add flathub repo in debian
+# 
+# Sway vnc config : 
+# rodhfr@Brotesco:~/.config/wayvnc$ cat config
+# display = 1
+# password = "password"
+# port = 5901
+
+git
